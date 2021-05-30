6cb050a4b9f6ee6e2b49eab9eba28bbe6600644a
haidang2408 <79898469+haidang2408@users.noreply.github.com>
2021-05-30 15:07:42 +0700

    Update bai2.py

--- bai2.py
+++ bai2.py
@@ -1,9 +1,10 @@
-import math;
-x1=("nhap x1 ")
-y1=("nhap y1 ")
-x2=("nhap x2 ")
-y2=("nhap y2 ")
+import math
+x1=int(input("nhap x1: "))
+y1=int(input("nhap y1: "))
+x2=int(input("nhap x2: "))
+y2=int(input("nhap y2: "))
 d1=(x2-x1)*(x2-x1)
-d2=(y2-y2)*(y2-y2)
+d2=(y2-y1)*(y2-y1)
 res=math.sqrt(d1+d2)
 print("kcach giua 2 diem la: ",res)
+
